550e3a8e8f5292f95b0534c39f21793961504b4d
Kevin M <crisstian2345@gmail.com>
2023-08-13 02:16:01 -0500

    agrega Map() para manejar varios tabIds

--- assets/background.js
+++ assets/background.js
@@ -1,16 +1,14 @@
-const audioContext = new AudioContext();
-const gainNode = audioContext.createGain();
-let sourceNode = null;
+const audioContexts = new Map();
 
 chrome.runtime.onMessage.addListener(function(request) {
     if(request.action === 'getCaptured'){
         getCapturedTags();
     }
     else if (request.action === "initCapture") {
-        initCapture(request.range);
+        initCapture(request.getTabId, request.range);
     }
     else if(request.action === 'deleteCapture'){
-        deleteCapture();
+        deleteCapture(request.getTabId);
     }
     else if(request.action === 'changeVolumen'){
         gainNode.gain.value = request.range;
@@ -21,17 +19,15 @@ function getCapturedTags(){
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         const currentTabId = tabs[0].id;
       
-        chrome.tabCapture.getCapturedTabs(function(capturedTabs) {
-            const isCurrentTabCaptured = capturedTabs.some(tab => tab.tabId === currentTabId);
-        
-            chrome.runtime.sendMessage({ action: 'showNotification', state: isCurrentTabCaptured });
-            console.log(isCurrentTabCaptured);
+        chrome.runtime.sendMessage({
+            action: 'showNotification',
+            tabId: currentTabId,
+            state: audioContexts.has(currentTabId) //false: se ejecutara initCapture() - true: se ejecutar deleteCapture()
         });
-        console.log(tabs, currentTabId);
     });
 }
 
-function initCapture(value) {
+function initCapture(tabId, value) {
     chrome.tabCapture.capture({
         audio: true,
         video: false,
@@ -41,23 +37,32 @@ function initCapture(value) {
             },
         },
     }, function(stream) {
-        sourceNode = audioContext.createMediaStreamSource(stream);
+        const audioContext = new AudioContext();
+        const sourceNode = audioContext.createMediaStreamSource(stream);
+        const gainNode = audioContext.createGain();
         gainNode.gain.value = value;
 
         sourceNode.connect(gainNode);
         gainNode.connect(audioContext.destination);
   
+        //guardar en Map()
+        audioContexts.set(tabId, {audioContext, sourceNode, gainNode});
+
         // Iniciar la reproducción del audio
         sourceNode.mediaStream.getAudioTracks()[0].enabled = true;
     });
 }
 
-function deleteCapture() {
-    if(sourceNode != null){
-        //detener la reproducción del audio and this tab's content is being shared
+function deleteCapture(tabId) {
+    //verifica que el tabId actual exista en Map()
+    if(audioContexts.has(tabId)){
+        const { audioContext, sourceNode, gainNode } = audioContexts.get(tabId);
+   
         sourceNode.mediaStream.getAudioTracks()[0].stop();
-
         sourceNode.disconnect(gainNode);
         gainNode.disconnect(audioContext.destination);
+        audioContext.close();
+
+        audioContexts.delete(tabId);
     }
 }
--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,32 @@
 const captureButton = document.getElementById('captureButton');
 const spliter = document.getElementById('rangeVolumen');
 let state = null;
+let tabId = null;
 
 document.addEventListener('DOMContentLoaded', () => {
     chrome.runtime.sendMessage({ action: 'getCaptured' });
     chrome.runtime.onMessage.addListener((message) => {
         if (message.action === "showNotification") {
-          alert("Se ha recibido una notificación desde el fondo." + message.state);
+          alert("Se ha recibido una notificación desde el fondo. " + message.state);
           state = message.state;
+          tabId = message.tabId;
         }
     });
 })
 
-
-
-
 captureButton.addEventListener('click', function() {
     // console.log(state);
+    if(state === null){
+        captureButton.textContent = 'Refresh!';
+        return;    
+    }
 
     if(state)
-        chrome.runtime.sendMessage({ action: "deleteCapture" });
+        chrome.runtime.sendMessage({ action: "deleteCapture", getTabId: tabId });
     else
-        chrome.runtime.sendMessage({ action: "initCapture", range: +spliter.value });
+        chrome.runtime.sendMessage({ action: "initCapture", getTabId: tabId, range: +spliter.value });
+
+    state = null;
 });
 
 spliter.addEventListener('change', function() {
